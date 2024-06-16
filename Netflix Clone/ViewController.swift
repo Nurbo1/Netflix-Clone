29d5be1dff60dd5e8bb49111ed04c47239962e20
Nurbol <nurbol23072002@gmail.com>
2024-06-16 09:16:13 +0500

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Нурбол Мухаметжан on 16.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
